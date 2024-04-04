6117335bd1e7c5b0818ee48cc016290a86d703c6
윤서희 <appleid21@sookmyung.ac.kr>
2024-04-04 16:03:00 +0900

    fix: if -> when문으로 수정

--- app/src/main/java/com/sopt/now/SignUpActivity.kt
+++ app/src/main/java/com/sopt/now/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.sopt.now
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Nickname
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.sopt.now.databinding.ActivitySignUpBinding
@@ -9,69 +10,66 @@ import com.sopt.now.databinding.ActivitySignUpBinding
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnSignup.setOnClickListener {
-            val idLength = binding.etSignupId.length()
-            val pwLength = binding.etSignupPw.length()
-            val nicknameLength = binding.etSignupNickname.length()
-            checkInput(idLength, pwLength, nicknameLength)
+            val userID = binding.etSignupId.text.toString()
+            val userPw = binding.etSignupPw.text.toString()
+            val userNickname = binding.etSignupNickname.text.toString()
+            checkInput(userID, userPw, userNickname)
         }
     }
 
-    private fun checkInput(idLength: Int, pwLength: Int, nicknameLength: Int) {
-        if (idLength in 6..10) {
-            if (pwLength in 8..12) {
-                if (nicknameLength >= 1) {
-                    if (!binding.etSignupNickname.text.isNullOrBlank()) {
-                        val intent = Intent()
-                        intent.putExtra("id", binding.etSignupId.text.toString())
-                        intent.putExtra("pw", binding.etSignupPw.text.toString())
-                        intent.putExtra("nickname", binding.etSignupNickname.text.toString())
-                        intent.putExtra("mbtiEI", binding.rgSignupEi.checkedRadioButtonId)
-                        intent.putExtra("mbtiSN", binding.rgSignupSn.checkedRadioButtonId)
-                        intent.putExtra("mbtiTF", binding.rgSignupTf.checkedRadioButtonId)
-                        intent.putExtra("mbtiJP", binding.rgSignupJp.checkedRadioButtonId)
-
-                        Snackbar.make(
-                            binding.root,
-                            "회원가입이 완료되었습니다",
-                            Snackbar.LENGTH_LONG
-                        ).show()
-
-                        setResult(RESULT_OK, intent)
-                        finish()
-                    }
-                    else {
-                        Snackbar.make(
-                            binding.root,
-                            "공백으로만 닉네임을 입력할 수 없습니다",
-                            Snackbar.LENGTH_SHORT
-                        ).show()
-                    }
-                } else {
-                    Snackbar.make(
-                        binding.root,
-                        "닉네임을 입력해주세요",
-                        Snackbar.LENGTH_SHORT
-                    ).show()
-                }
-            } else {
-                Snackbar.make(
-                    binding.root,
-                    "비밀번호는 8자 이상 12자 이하여야 합니다",
-                    Snackbar.LENGTH_SHORT
-                ).show()
-            }
-        } else {
-            Snackbar.make(
-                binding.root,
-                "아이디는 6자 이상 10자 이하여야 합니다",
-                Snackbar.LENGTH_SHORT
-            ).show()
+    private fun checkInput(userID: String, userPw: String, userNickname: String) {
+        return when {
+            userID.length < 6 || userID.length > 10 -> showMessageIdLength()
+            userPw.length < 8 || userPw.length > 12 -> showMessagePwLength()
+            userNickname.isNullOrBlank() -> showMessageNickname()
+            else -> checkedSignup()
         }
+    }
+
+    private fun checkedSignup() {
+        val intent = Intent()
+        intent.putExtra("id", binding.etSignupId.text.toString())
+        intent.putExtra("pw", binding.etSignupPw.text.toString())
+        intent.putExtra("nickname", binding.etSignupNickname.text.toString())
+
+        Snackbar.make(
+            binding.root,
+            "회원가입이 완료되었습니다",
+            Snackbar.LENGTH_LONG
+        ).show()
+
+        setResult(RESULT_OK, intent)
+        finish()
+    }
+
+    private fun showMessageIdLength() {
+        Snackbar.make(
+            binding.root,
+            "아이디는 6자 이상 10자 이하여야 합니다",
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun showMessagePwLength() {
+        Snackbar.make(
+            binding.root,
+            "비밀번호는 8자 이상 12자 이하여야 합니다",
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun showMessageNickname() {
+        Snackbar.make(
+            binding.root,
+            "닉네임을 입력해주세요",
+            Snackbar.LENGTH_SHORT
+        ).show()
     }
 }
