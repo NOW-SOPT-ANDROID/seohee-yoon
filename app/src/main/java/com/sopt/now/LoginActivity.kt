fec6b2f1c8f1ff10826bf2635d4f029c42b833e0
윤서희 <appleid21@sookmyung.ac.kr>
2024-04-04 15:46:16 +0900

    fix: 초기화 로직
    resultLauncher 외에서 로직 수정

--- app/src/main/java/com/sopt/now/LoginActivity.kt
+++ app/src/main/java/com/sopt/now/LoginActivity.kt
@@ -3,65 +3,74 @@ package com.sopt.now
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 import com.sopt.now.databinding.ActivityLoginBinding
-import com.sopt.now.databinding.ActivitySignUpBinding
-import kotlin.properties.Delegates
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+
+    private var userId:String ?= null
+    private var userPw:String ?= null
+    private var userNickname:String ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        getResultSignup()
+        goToSignup()
+        clickLogin()
+    }
+
+    private fun getResultSignup() {
         resultLauncher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == RESULT_OK) {
                 val data: Intent? = result.data
 
-                val id = data?.getStringExtra("id")
-                val pw = data?.getStringExtra("pw")
-                val nickname = data?.getStringExtra("nickname")
+                userId = data?.getStringExtra("id")
+                userPw = data?.getStringExtra("pw")
+                userNickname = data?.getStringExtra("nickname")
 
-                binding.etLoginId.setText(id)
-                binding.etLoginPw.setText(pw)
-
-                binding.btnLogin.setOnClickListener{
-                    if (id == binding.etLoginId.text.toString() && pw == binding.etLoginPw.text.toString()) {
-                        Snackbar.make(
-                            binding.root,
-                            "로그인 성공",
-                            Snackbar.LENGTH_LONG
-                        ).show()
-
-                        val intent = Intent(this, MainActivity::class.java)
-                        intent.putExtra("id", id)
-                        intent.putExtra("pw", pw)
-                        intent.putExtra("nickname", nickname)
-                        startActivity(intent)
-                    } else {
-                        Snackbar.make(
-                            binding.root,
-                            "아이디 또는 비밀번호가 일치하지 않습니다",
-                            Snackbar.LENGTH_SHORT
-                        ).show()
-                    }
-                }
+                binding.etLoginId.setText(userId)
+                binding.etLoginPw.setText(userPw)
             }
         }
+    }
 
+    private fun goToSignup() {
         binding.btnSignup.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             resultLauncher.launch(intent)
+        }
+    }
+
+    private fun clickLogin() {
+        binding.btnLogin.setOnClickListener{
+            if (userId == binding.etLoginId.text.toString() && userPw == binding.etLoginPw.text.toString()) {
+                Snackbar.make(
+                    binding.root,
+                    "로그인 성공",
+                    Snackbar.LENGTH_LONG
+                ).show()
+
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("id", userId)
+                intent.putExtra("pw", userPw)
+                intent.putExtra("nickname", userNickname)
+                startActivity(intent)
+            } else {
+                Snackbar.make(
+                    binding.root,
+                    "아이디 또는 비밀번호가 일치하지 않습니다",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 }
