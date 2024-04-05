e2a359a7045a83d798f23ba46d48d9b9e646f8fe
윤서희 <appleid21@sookmyung.ac.kr>
2024-04-05 16:34:38 +0900

    feat: 회원가입 유효성 검사

--- app/src/main/java/com/sopt/now/compose/SignUpActivity.kt
+++ app/src/main/java/com/sopt/now/compose/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.sopt.now.compose
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +11,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -17,13 +22,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
+import kotlinx.coroutines.launch
 
 class SignUpActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,77 +58,121 @@ fun SignUp() {
     var nickname by remember { mutableStateOf("") }
     var mbti by remember { mutableStateOf("") }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(30.dp)
-    ) {
-        Text(
-            text = "Sign Up",
-            fontSize = 28.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth()
-        )
-        Text(
-            text="ID",
-            fontSize = 24.sp,
-            modifier = Modifier.padding(top = 30.dp)
-        )
-        TextField(
-            value = userId,
-            onValueChange = {userId = it},
-            label = { Text("아이디를 입력해주세요") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 12.dp)
-        )
-        Text(
-            text="비밀번호",
-            fontSize = 24.sp,
-            modifier = Modifier.padding(top = 30.dp)
-        )
-        TextField(
-            value = password,
-            onValueChange = {password = it},
-            label = { Text("비밀번호를 입력해주세요") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 12.dp)
-        )
-        Text(
-            text="닉네임",
-            fontSize = 24.sp,
-            modifier = Modifier.padding(top = 30.dp)
-        )
-        TextField(
-            value = nickname,
-            onValueChange = {nickname = it},
-            label = { Text("닉네임을 입력해주세요") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 12.dp)
-        )
-        Text(
-            text="MBTI",
-            fontSize = 24.sp,
-            modifier = Modifier.padding(top = 30.dp)
-        )
-        TextField(
-            value = mbti,
-            onValueChange = {mbti = it},
-            label = { Text("MBTI를 입력해주세요") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 12.dp)
-        )
-        Spacer(modifier = Modifier.weight(1f))
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+    val context = LocalContext.current
 
-        Button(
-            onClick = {  },
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { innerPadding ->
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 30.dp)) {
-            Text(text = "회원가입 하기")
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(30.dp)
+        ) {
+            Text(
+                text = "Sign Up",
+                fontSize = 28.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Text(
+                text="ID",
+                fontSize = 24.sp,
+                modifier = Modifier.padding(top = 30.dp)
+            )
+            TextField(
+                value = userId,
+                onValueChange = {userId = it},
+                label = { Text("아이디를 입력해주세요") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp)
+            )
+            Text(
+                text="비밀번호",
+                fontSize = 24.sp,
+                modifier = Modifier.padding(top = 30.dp),
+            )
+            TextField(
+                value = password,
+                onValueChange = {password = it},
+                label = { Text("비밀번호를 입력해주세요") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp)
+            )
+            Text(
+                text="닉네임",
+                fontSize = 24.sp,
+                modifier = Modifier.padding(top = 30.dp)
+            )
+            TextField(
+                value = nickname,
+                onValueChange = {nickname = it},
+                label = { Text("닉네임을 입력해주세요") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp)
+            )
+            Text(
+                text="MBTI",
+                fontSize = 24.sp,
+                modifier = Modifier.padding(top = 30.dp)
+            )
+            TextField(
+                value = mbti,
+                onValueChange = {mbti = it},
+                label = { Text("MBTI를 입력해주세요") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+
+            Button(
+                onClick = {
+                    when {
+                        userId.length !in 6..10 -> {
+                            coroutineScope.launch {
+                                snackbarHostState.showSnackbar(
+                                "아이디는 6자 이상 10자 이하여야 합니다",
+                                duration = SnackbarDuration.Short)
+                            }
+                        }
+                        password.length !in 8..12 -> {
+                            coroutineScope.launch {
+                                snackbarHostState.showSnackbar(
+                                "비밀번호는 8자 이상 16자 이하여야 합니다",
+                                duration = SnackbarDuration.Short)
+                            }
+                        }
+                        nickname.isBlank() -> {
+                            coroutineScope.launch {
+                                snackbarHostState.showSnackbar(
+                                "닉네임을 입력해주세요",
+                                duration = SnackbarDuration.Short)
+                            }
+                        }
+                        else -> {
+                            coroutineScope.launch {
+                                snackbarHostState.showSnackbar(
+                                "회원가입 성공",
+                                duration = SnackbarDuration.Long)
+                            }
+
+                            context.startActivity(Intent(context, LoginActivity::class.java))
+                        }
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp, bottom = 12.dp)) {
+                Text(text = "회원가입 하기")
+            }
         }
     }
 }
+
