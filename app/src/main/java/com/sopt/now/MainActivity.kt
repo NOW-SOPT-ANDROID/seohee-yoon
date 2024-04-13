3dfab37e0309b939af54796faf14157c6eb00827
윤서희 <appleid21@sookmyung.ac.kr>
2024-04-14 02:16:13 +0900

    feat: bottom navigation과 fragment 연결

--- app/src/main/java/com/sopt/now/MainActivity.kt
+++ app/src/main/java/com/sopt/now/MainActivity.kt
@@ -2,9 +2,7 @@ package com.sopt.now
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.sopt.now.LoginActivity.Companion.ID
-import com.sopt.now.LoginActivity.Companion.NICKNAME
-import com.sopt.now.LoginActivity.Companion.PW
+import androidx.fragment.app.Fragment
 import com.sopt.now.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,14 +12,42 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        getData()
+        val currentFragment = supportFragmentManager.findFragmentById(binding.fcvMain.id)
+        if (currentFragment == null) {
+            supportFragmentManager.beginTransaction()
+                .add(binding.fcvMain.id, HomeFragment())
+                .commit()
+        }
+
+        clickBottomNavigation()
     }
 
-    private fun getData() {
-        intent.apply {
-            binding.tvMainNickname.text = getStringExtra(NICKNAME)
-            binding.tvMainId.text = getStringExtra(ID)
-            binding.tvMainPw.text = getStringExtra(PW)
+    private fun clickBottomNavigation() {
+        binding.bnvMain.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.menu_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+
+                R.id.menu_search -> {
+                    replaceFragment(SearchFragment())
+                    true
+                }
+
+                R.id.menu_mypage -> {
+                    replaceFragment(MyPageFragment())
+                    true
+                }
+
+                else -> false
+            }
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction() // Activity에서 Fragment를 다루기 때문
+            .replace(R.id.fcv_main, fragment)
+            .commit()
     }
 }
