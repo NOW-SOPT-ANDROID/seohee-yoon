961afd4e5c06736392170576dc904b3444428788
윤서희 <appleid21@sookmyung.ac.kr>
2024-05-03 16:19:44 +0900

    feat: #9 mypage 통신

--- app/src/main/java/com/sopt/now/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/sopt/now/ui/mypage/MyPageFragment.kt
@@ -2,19 +2,23 @@ package com.sopt.now.ui.mypage
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.sopt.now.data.User
+import androidx.fragment.app.viewModels
+import com.sopt.now.data.Key.USER_ID
 import com.sopt.now.databinding.FragmentMyPageBinding
 import com.sopt.now.ui.login.LoginActivity
 
 
-class MyPageFragment(private val user: User) : Fragment() {
+class MyPageFragment() : Fragment() {
     private var _binding: FragmentMyPageBinding? = null
     private val binding: FragmentMyPageBinding
         get() = _binding ?: throw IllegalStateException("Binding is null")
+
+    private val viewModel by viewModels<MyPageViewModel>()
 
 
     override fun onCreateView(
@@ -27,16 +31,30 @@ class MyPageFragment(private val user: User) : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         initViews()
         initLogoutBtnClickListener()
     }
 
     private fun initViews() {
-        binding.apply {
-            tvMypageNickname.text = user.name
-            tvMypageId.text = user.id
-            tvMypagePw.text = user.password
-            tvMypagePhone.text = user.phone
+        val userId = activity?.intent?.getStringExtra(USER_ID)
+
+        if (userId != null) {
+            viewModel.getUserData(userId)
+
+            binding.apply {
+                viewModel.userData.observe(viewLifecycleOwner) { userData ->
+                    if (userData != null) {
+                        tvMypageId.text = userData.authenticationId
+                        tvMypageNickname.text = userData.nickname
+                        tvMypagePhone.text = userData.phone
+                    } else {
+                        Log.d("MyPageFragment", "User data is null.")
+                    }
+                }
+            }
+        } else {
+            Log.d("MyPageFragment", "User ID is null.")
         }
     }
 
---
+++ app/src/main/java/com/sopt/now/ui/mypage/MyPageViewModel.kt
@@ -0,0 +1,37 @@
+package com.sopt.now.ui.mypage
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sopt.now.data.ServicePool
+import com.sopt.now.data.dto.response.ResponseUserDto
+import com.sopt.now.data.dto.response.ResponseUserDto.UserData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MyPageViewModel() : ViewModel() {
+    var userData = MutableLiveData<UserData?>()
+
+    fun getUserData(userId: String) {
+        ServicePool.userService.getUser(userId).enqueue(object : Callback<ResponseUserDto> {
+            override fun onResponse(
+                call: Call<ResponseUserDto>,
+                response: Response<ResponseUserDto>
+            ) {
+                if (response.isSuccessful) {
+                    val data: ResponseUserDto? = response.body()
+                    userData.value = data?.data
+
+                    Log.d("MyPageViewModel", "User data: ${userData.value}")
+                } else {
+                    Log.d("MyPageViewModel", "Failed to load user data: $userId")
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseUserDto>, t: Throwable) {
+                Log.d("MyPageViewModel", "Failed to load user data: ${t.message}")
+            }
+        })
+    }
+}
