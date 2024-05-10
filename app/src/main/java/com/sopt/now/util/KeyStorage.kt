879adc495a25d75d841c6bf2cedcbf9bace0ab9a
윤서희 <appleid21@sookmyung.ac.kr>
2024-05-10 14:34:27 +0900

    [delete] 불필요한 코드 삭제

--- app/src/main/java/com/sopt/now/util/KeyStorage.kt
+++ app/src/main/java/com/sopt/now/util/KeyStorage.kt
@@ -1,8 +1,7 @@
-package com.sopt.now.data
+package com.sopt.now.util
 
 object KeyStorage {
     const val USER_PREF = "user_pref"
-    const val USER_ID = "userId"
     const val MEMBER = "member"
     const val MEMBER_ID = "memberId"
     const val JOIN = "join"
